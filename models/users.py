2c5a263d3ef62d4dc39f39c0d14a9a627ea65d49
Aleksei Khatkevich <qwerty12345@disroot.org>
2023-05-25 17:58:58 +0300

    Users table initial

--- models/users.py
+++ models/users.py
@@ -1,7 +1,7 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, String, BigInteger, DateTime, TIMESTAMP
+from sqlalchemy.sql import func
 
 from database import Base
-
 
 __all__ = (
     'User',
@@ -9,9 +9,39 @@ __all__ = (
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    id = Column(
+        BigInteger,
+        primary_key=True,
+        index=True,
+    )
+    name = Column(
+        String(64),
+        nullable=False,
+    )
+    email = Column(
+        String(320),
+        unique=True,
+        index=True,
+    )  #todo check constraint
+    hashed_password = Column(
+        String(256),
+        nullable=False,
+    )
+    is_active = Column(
+        Boolean,
+        default=True,
+    )
+    registration_date = Column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )# todo в какой таймзоне сохраняет
+    updated_at = Column(
+        TIMESTAMP(timezone=True),
+        onupdate=func.now(),
+    )
+
+    def __str__(self):
+        return f'User with {self.id=}'
