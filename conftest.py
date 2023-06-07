ab69abd569b99393f21aba6c690a74f03fddbf89
Aleksei Khatkevich <qwerty12345@disroot.org>
2023-06-07 15:44:25 +0300

    Add new caplog fixture

--- conftest.py
+++ conftest.py
@@ -2,8 +2,10 @@ import asyncio
 from typing import TYPE_CHECKING
 
 import pytest
+from _pytest.logging import LogCaptureFixture
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
+from loguru import logger
 from sqlalchemy import text
 
 from internal.database import Base, async_session
@@ -17,6 +19,22 @@ if TYPE_CHECKING:
 pytest_plugins = [
     'tests.users.fixtures',
 ]
+
+
+@pytest.fixture
+def caplog(caplog: LogCaptureFixture):
+    """
+    https://loguru.readthedocs.io/en/stable/resources/migration.html#replacing-caplog-fixture-from-pytest-library
+    """
+    handler_id = logger.add(
+        caplog.handler,
+        format="{message}",
+        level=0,
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        enqueue=False,  # Set to 'True' if your test is spawning child processes.
+    )
+    yield caplog
+    logger.remove(handler_id)
 
 
 @pytest.fixture
