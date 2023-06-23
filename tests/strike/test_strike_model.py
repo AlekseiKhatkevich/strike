5bbe2dc2e72721cda0c0e03437401c5715e699d2
Aleksei Khatkevich <qwerty12345@disroot.org>
2023-06-23 12:16:47 +0300

    Test on Strike 6

--- tests/strike/test_strike_model.py
+++ tests/strike/test_strike_model.py
@@ -1,16 +1,14 @@
-import datetime
-
 import pytest
 from sqlalchemy import update
+from sqlalchemy.exc import IntegrityError
 
 from crud.helpers import exists_in_db
-from models import Strike
-from sqlalchemy.exc import IntegrityError
+from models import Strike, StrikeToItself
 
 
 async def test_strike_positive_basic(db_session, strike: Strike):
     """
-
+    Позитивный тест модели Strike. Должна быть создана запись в БД в случае если все ок.
     """
     assert await exists_in_db(db_session, Strike, Strike.id == strike.id)
     assert None not in {
@@ -26,7 +24,8 @@ async def test_strike_positive_basic(db_session, strike: Strike):
 
 async def test_strike_negative_model_level_validation(db_session, strike: Strike):
     """
-
+    Негативный тест модели Strike. Если поле overall_num_of_employees_involved < 1 то
+    должно быть возбужденно исключение.
     """
     with pytest.raises(ValueError):
         strike.overall_num_of_employees_involved = -1
@@ -34,7 +33,8 @@ async def test_strike_negative_model_level_validation(db_session, strike: Strike
 
 async def test_strike_negative_overall_num_of_employees_involved_positive(db_session, strike: Strike):
     """
-
+    Негативный тест модели Strike. Если поле overall_num_of_employees_involved < 1 то
+    должно быть возбужденно исключение (на уровне БД).
     """
     with pytest.raises(IntegrityError, match='overall_num_of_employees_involved_positive'):
         stmt = update(Strike).where(Strike.id == strike.id).values(
@@ -45,7 +45,8 @@ async def test_strike_negative_overall_num_of_employees_involved_positive(db_ses
 
 async def test_strike_negative_one_of_dates_is_not_null(strike_p):
     """
-
+    Негативный тест модели Strike. Если оба 2 поля planned_on_date и duration Null -
+    то возбуждается исключение на уровне БД.
     """
     with pytest.raises(IntegrityError, match='one_of_dates_is_not_null'):
         await strike_p(duration=None)
@@ -53,7 +54,25 @@ async def test_strike_negative_one_of_dates_is_not_null(strike_p):
 
 async def test_strike_negative_duration_enterprise_exc_constraint(db_session, strike: Strike, strike_p):
     """
-
+    Негативный тест модели Strike. Если диапазон в duration пересекает такой же диапазон
+    в какой-либо другой записи модели Strike в БД и у обоих этих двух записей одинаковая
+    компания - то возбуждается исключение на уровне БД.
     """
     with pytest.raises(IntegrityError, match='duration_enterprise_exc_constraint'):
         await strike_p(enterprise=strike.enterprise, duration=strike.duration)
+
+
+async def test_strike_group(db_session, strike, strike_p):
+    """
+    Позитивный тест модели Strike. Возможность создавать группы забастовок через м2м на себя.
+    """
+    strike2 = await strike_p()
+    group = await strike.awaitable_attrs.group
+    group.append(strike2)
+    await db_session.commit()
+
+    assert await exists_in_db(
+        db_session,
+        StrikeToItself,
+        (StrikeToItself.strike_left_id == strike.id) & (StrikeToItself.strike_right_id == strike2.id)
+    )
