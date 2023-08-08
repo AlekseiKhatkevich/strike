f47cd791fde09658f164979b7ae2ada7e4dca05d
Aleksei Khatkevich <qwerty12345@disroot.org>
2023-08-08 14:41:06 +0300

    Graphql try | 2

--- graphql_related/main.py
+++ graphql_related/main.py
@@ -5,19 +5,20 @@ from sqlalchemy import func, select
 from strawberry.fastapi import GraphQLRouter
 import dataclasses
 from internal.database import async_session
-from models.detention import Detention as DetentionModel
+from models.detention import Detention as DetentionModel, Jail as JailModel
 
 __all__ = (
     'graphql_app',
 )
 
 
-async def get_detentions():
+async def get_detentions(root) -> list['Detention']:
     async with async_session() as session:
         data = await session.scalars(
             select(DetentionModel).order_by(func.lower(DetentionModel.duration).desc())
         )
-        f_names = [f.name for f in dataclasses.fields(Detention)]
+        exc = {'duration', 'jail'}
+        f_names = [f.name for f in dataclasses.fields(Detention) if f.name not in exc]
         detentions = []
         for det in data:
             data = {f_name: getattr(det, f_name) for f_name in f_names}
@@ -26,10 +27,29 @@ async def get_detentions():
         return detentions
 
 
+async def get_jail_for_detention(root):
+    async with async_session() as session:
+        j = await session.get(JailModel, root.jail_id)
+        return Jail(
+            id=j.id,
+            name=j.name,
+            address=j.address,
+            region_id=j.region_id,
+        )
+
+
 @strawberry.type
 class Duration:
     lower: datetime.datetime
     upper: datetime.datetime | None
+
+
+@strawberry.type
+class Jail:
+    id: int
+    name: str
+    address: str
+    region_id: str
 
 
 @strawberry.type
@@ -44,6 +64,22 @@ class Detention:
     charge: str | None
     transferred_from_id: int | None
     relative_or_friend: str | None
+    jail: Jail = strawberry.field(resolver=get_jail_for_detention)
+
+
+@strawberry.input
+class ZKTransferInput:
+    detention_id: int = strawberry.field(description='Who is transferred')
+    jail: int = strawberry.field(description='Transfer poor motherfucker where exactly')
+    transfer_dt: datetime.datetime = strawberry.field(description='Transfer datetime')
+    detention_upper: datetime.datetime | None = strawberry.field(description='Extend cell time if needed.')
+
+
+@strawberry.type
+class Mutation:
+    @strawberry.field
+    async def zk_transfer(self, transfer: ZKTransferInput) -> Detention:
+       return 2
 
 
 @strawberry.type
@@ -51,6 +87,6 @@ class Query:
     detentions: list[Detention] = strawberry.field(resolver=get_detentions)
 
 
-schema = strawberry.Schema(Query)
+schema = strawberry.Schema(Query, Mutation)
 
 graphql_app = GraphQLRouter(schema)
