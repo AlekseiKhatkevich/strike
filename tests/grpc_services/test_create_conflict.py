5c4c7c63a907c9f0eae4f0a54a08624053f8ec63
Aleksei Khatkevich <qwerty12345@disroot.org>
2023-08-18 14:37:37 +0300

    GRPC tests | CreateConflict

---
+++ tests/grpc_services/test_create_conflict.py
@@ -0,0 +1,34 @@
+import grpc
+
+from crud.helpers import exists_in_db
+from internal.database import async_session
+from models import Conflict
+from serializers.proto.compiled import conflicts_pb2
+
+
+async def test_CreateConflict_positive(get_grpc_response, conflict_factory, enterprise_factory):
+    """
+    Позитивный тест метода CreateConflict создания записи конфликта в БД.
+    """
+    async with async_session() as session:
+        enterprise = enterprise_factory.build()
+        session.add(enterprise)
+        await session.commit()
+
+        conflict = conflict_factory.build(enterprise=enterprise)
+        request = conflicts_pb2.Conflict(
+            type=conflict.type.name,
+            enterprise_id=conflict.enterprise.id,
+            description=conflict.description,
+        )
+        request.duration.lower.FromDatetime(conflict.duration.lower)
+        request.duration.upper.FromDatetime(conflict.duration.upper)
+
+        response, _, code, _ = await get_grpc_response('CreateConflict', request)
+
+        assert code == grpc.StatusCode.OK
+        pb_enum_name = conflicts_pb2.ConflictTypes.Name(response.conflict.type)
+        assert pb_enum_name == conflict.type.name
+        assert response.conflict.id is not None
+        assert isinstance(response.conflict, conflicts_pb2.Conflict)
+        assert await exists_in_db(session, Conflict, Conflict.id == response.conflict.id)
