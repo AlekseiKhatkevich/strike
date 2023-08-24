b869e903aa797a60a8532dd0943cf1647633869c
Aleksei Khatkevich <qwerty12345@disroot.org>
2023-08-24 14:58:58 +0300

    Kafka | producer | Prettify

--- kafka_related/producer.py
+++ kafka_related/producer.py
@@ -1,14 +1,20 @@
 import asyncio
+import contextlib
 import heapq
 import random
 from itertools import count
+from typing import AsyncIterator, Iterable
+
 from confluent_kafka import Producer
 from confluent_kafka.admin import AdminClient
-from confluent_kafka.cimpl import NewPartitions, NewTopic
+from confluent_kafka.cimpl import KafkaException, NewPartitions, NewTopic
+from loguru import logger
 from shapely import Point
 from shapely.geometry import LineString
 
 from serializers.for_kafka import KafkaCoordinatesSerializer
+
+partitions_lock = asyncio.Lock()
 
 config = {
     'bootstrap.servers': '127.0.0.1:29092',
@@ -25,7 +31,10 @@ coordinates_topic = NewTopic(
 
 
 class KafkaPointsProducer:
-    def __init__(self, num_users=None, topic=coordinates_topic):
+    """
+    Пишет координаты юзера по ходу его маршрута в Кафку.
+    """
+    def __init__(self, num_users: int | None = None, topic: NewTopic = coordinates_topic) -> None:
         self.producer = Producer(config)
         self.admin = AdminClient(config)
         self.topic = topic
@@ -37,15 +46,25 @@ class KafkaPointsProducer:
         ]
         self._configure()
 
-    def _configure(self):
-        self.admin.create_topics([self.topic])
+    def _configure(self) -> None:
+        """
+        Начальная конфигурация брокера и топика.
+        1) Создаем топик с 10ю начальными партициями.
+        """
+        self.admin.create_topics([self.topic], request_timeout=0.05)
 
     @property
     def _partitions_count(self) -> int:
+        """
+        Кол-во партиций топика на текущий момент времени.
+        """
         return len(self.producer.list_topics().topics[self.topic.topic].partitions)
 
     @staticmethod
-    def generate_random_start_and_stop():
+    def generate_random_start_and_stop() -> tuple[Point, Point]:
+        """
+        Рандомные точки начала и окончания маршрута.
+        """
         start_lat = random.uniform(-90, 90)
         start_lon = random.uniform(-180, 180)
         end_lat = random.uniform(start_lat - 0.01, start_lat + 0.01)
@@ -53,15 +72,23 @@ class KafkaPointsProducer:
 
         return Point(start_lon, start_lat), Point(end_lon, end_lat)
 
-    async def _do_job(self, route):
+    async def _send_one_event(self, route: 'RandomRoute') -> None:
+        """
+        Отправляет 1 эвент с координатами юзера на Кафку.
+        """
         async for point in route.produce_points():
-            print(f'User_id = {route.user_id}, point= {point}')
             if route.user_id not in self.known_user_ids:
+                #  Присваиваем конкретному user_id свой partition_id
                 self.known_user_ids[route.user_id] = next(self.used_partitions_counter)
-                self.admin.create_partitions(
-                    [NewPartitions(self.topic.topic, self._partitions_count + 1)],
-                    request_timeout=0.05,
-                )
+                async with partitions_lock:
+                    resp = self.admin.create_partitions(
+                        [NewPartitions(self.topic.topic, len(self.known_user_ids) + 3)],
+                        request_timeout=0.05,
+                    )
+                    # Ждем пока создастся партиция перед тем как продолжить
+                    # KafkaException - если партиция уже существует - но нам это ок
+                    with contextlib.suppress(KafkaException, TimeoutError):
+                        resp[self.topic.topic].result(0.5)
             if point:
                 data_serializer = KafkaCoordinatesSerializer(user_id=route.user_id, point=point)
                 self.producer.produce(
@@ -70,30 +97,55 @@ class KafkaPointsProducer:
                     value=data_serializer.model_dump_json(),
                     partition=self.known_user_ids[route.user_id],
                 )
+                logger.info(
+                    f'User_id = {route.user_id}, point = {point}, partition = {self.known_user_ids[route.user_id]}'
+                )
 
-    async def produce(self):
-        coros = [self._do_job(route) for route in self.routes]
+    async def produce(self) -> None:
+        """
+        Запускаем всю процедуру.
+        """
         self.producer.poll(0)
-        await asyncio.gather(*coros)
+        await asyncio.gather(*(self._send_one_event(route) for route in self.routes))
+        logger.info('Done!')
 
 
 class OriginDistanceHeap:
-    def __init__(self, random_points, *, first_point):
+    """
+    Куча, которая хранит набор точек (Shapely Point) отсортированными по дальности
+    от какой-то начальной точки.
+    """
+    def __init__(self, random_points: Iterable[Point], *, first_point: Point) -> None:
         self.heap = [
             (first_point.distance(p), p) for p in random_points
         ]
         heapq.heapify(self.heap)
 
-    def pop(self):
+    def pop(self) -> tuple[float, Point]:
+        """
+        POP() одного элемента из кучи самого близкого к первоначальной точке.
+        """
         return heapq.heappop(self.heap)
 
     @property
     def is_empty(self):
+        """
+        Есть ли в куче элементы еше.
+        """
         return not bool(self.heap)
 
 
 class RandomRoute:
-    def __init__(self, first_point, last_point, *, user_id, num_points=None):
+    """
+    Генерирует рандомный маршрут из последовательных точек.
+    """
+    def __init__(self,
+                 first_point: Point,
+                 last_point: Point,
+                 *,
+                 user_id: int,
+                 num_points: int | None = None,
+                 ):
         self.first_point = first_point
         self.last_point = last_point
         self.user_id = user_id
@@ -102,7 +154,11 @@ class RandomRoute:
         random_points = self._produce_random_points(num_points)
         self.heap = OriginDistanceHeap(random_points, first_point=first_point)
 
-    def _produce_random_points(self, num_points):
+    def _produce_random_points(self, num_points: int) -> list[Point, ...]:
+        """
+        Создает НЕ ОТСОРТИРОВАННЫЙ по удалению от первоначальной точки набор точек пути
+        вдоль линии соединяющей первую и последнюю точку маршрута.
+        """
         line = LineString([self.first_point, self.last_point])
 
         intermediate_points = []
@@ -112,7 +168,11 @@ class RandomRoute:
 
         return intermediate_points
 
-    async def produce_points(self):
+    async def produce_points(self) -> AsyncIterator[Point]:
+        """
+        Отдает точки маршрута последовательно начиная от начальной точки, затем спит
+        от 0,5 до 3 секунд.
+        """
         while not self.heap.is_empty:
             _, point = self.heap.pop()
             yield point
