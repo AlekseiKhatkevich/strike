8c35eabc0f7dd20d2b535cc476c8a2e280018d8d
Aleksei Khatkevich <qwerty12345@disroot.org>
2023-08-25 14:55:59 +0300

    Kafka | consumer | 3

--- kafka_related/consumer.py
+++ kafka_related/consumer.py
@@ -1,63 +1,76 @@
 import asyncio
 from collections import defaultdict
-
-from confluent_kafka import Consumer
+import orjson
+from aiokafka import AIOKafkaConsumer
 from loguru import logger
 
-conf = {
-    'bootstrap.servers': '127.0.0.1:29092',
-    'group.id': 'coordinates_consumers1',
-    'auto.offset.reset': 'earliest',
-    'enable.auto.commit': True,
-    'heartbeat.interval.ms': 3000,
-}
+from serializers.for_kafka import KafkaCoordinatesDeSerializer
+
+# conf = {
+#     'bootstrap.servers': '127.0.0.1:29092',
+#     'group.id': 'coordinates_consumers1',
+#     'auto.offset.reset': 'earliest',
+#     'enable.auto.commit': True,
+#     'heartbeat.interval.ms': 3000,
+# }
+aioconf = dict(
+    bootstrap_servers='127.0.0.1:29092',
+    group_id='coordinates_consumers1',
+    enable_auto_commit=True,
+    auto_commit_interval_ms=100,
+    auto_offset_reset='earliest',
+)
+
+
+class CoordinatesStorage:
+    """
+
+    """
+    def __init__(self):
+        self._storage = defaultdict(list)
+
+    def add(self, user_id, ser):
 
 
 class KafkaCoordinatesConsumer:
     """
     """
-
     def __init__(self, cons_qty, poll_delay=1):
         self.cons_qty = cons_qty
         self.poll_delay = poll_delay
         self.cons_user_id_map = defaultdict(set)
-        self.consumers = [Consumer(conf) for _ in range(self.cons_qty)]
-        for cons in self.consumers:
-            cons.subscribe(['coordinates'])
+        self.storage = CoordinatesStorage()
+
+    def _handle_one_coord(self, msg):
+        data = orjson.loads(msg.value)
+        ser = KafkaCoordinatesDeSerializer(**data)
+        self.storage
 
     async def _one_consumer(self, number, consumer):
         logger.info(f'Starting consumer # {number}')
-
-        while True:
-            await asyncio.sleep(0.0)
-            msg = consumer.poll(0.1)
-            if msg is None:
-                logger.warning(f'Consumer # {number}, Could not get message')
-                await asyncio.sleep(self.poll_delay)
-                continue
-            elif err := msg.error():
-                logger.error(f"Consumer error happened: {err}")
-                continue
-            else:
+        await consumer.start()
+        try:
+            async for msg in consumer:
                 logger.info(
                     f'Consumer # {number},'
-                    f' partition # {msg.partition()},'
-                    f' key # {msg.key()},'
-                    f' offset # {msg.offset()},'
-                    f' value - {msg.value()}'
+                    f' partition # {msg.partition},'
+                    f' key # {msg.key},'
+                    f' offset # {msg.offset},'
+                    f' value - {msg.value}'
                 )
-                self.cons_user_id_map[number].add(msg.key())
+                self.cons_user_id_map[number].add(msg.key)
+        finally:
+            await consumer.stop()
+            logger.info(f'Consumer # {number} has stopped')
 
     async def consume(self):
         coros = []
-        for num, cons in enumerate(self.consumers):
-            coro = self._one_consumer(num, cons)
+        for num in range(self.cons_qty):
+            consumer = AIOKafkaConsumer(
+                'coordinates',
+                **aioconf,
+            )
+            coro = self._one_consumer(num, consumer)
             coros.append(coro)
 
-        try:
-            await asyncio.gather(*coros)
-        finally:
-            for cons in self.consumers:
-                cons.close()
-                logger.info(f'Closing consumer {cons}')
-
+        await asyncio.gather(*coros)
--- serializers/for_kafka.py
+++ serializers/for_kafka.py
@@ -1,12 +1,13 @@
-from pydantic import ConfigDict
+from pydantic import ConfigDict, field_validator
 
-from shapely import Point
+from shapely import Point, wkt
 
 from internal.serializers import BaseModel
 from serializers.typing import IntIdType
 
 __all__ = (
     'KafkaCoordinatesSerializer',
+    'KafkaCoordinatesDeSerializer',
 )
 
 
@@ -21,3 +22,17 @@ class KafkaCoordinatesSerializer(BaseModel):
         arbitrary_types_allowed=True,
         json_encoders={Point: lambda p: p.wkt},
     )
+
+
+class KafkaCoordinatesDeSerializer(KafkaCoordinatesSerializer):
+    """
+    Для десериализации данных послед кафки.
+    """
+    # noinspection PyNestedDecorators
+    @field_validator('point', mode='before')
+    @classmethod
+    def _deserialize_point(cls, point: str) -> Point:
+        """
+        Десериализуем строковый point в shapely POINT.
+        """
+        return wkt.loads(point)
