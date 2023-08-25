fdbe2a90c4aa7f9b0f71550aa30e7557cb3f3547
Aleksei Khatkevich <qwerty12345@disroot.org>
2023-08-25 13:28:23 +0300

    Kafka | consumer | 2

--- kafka_related/consumer.py
+++ kafka_related/consumer.py
@@ -1,39 +1,42 @@
-from confluent_kafka import Consumer
-import time
 import asyncio
-from loguru import logger
+from collections import defaultdict
 
+from confluent_kafka import Consumer
+from loguru import logger
 
 conf = {
     'bootstrap.servers': '127.0.0.1:29092',
-    'group.id': 'coordinates_consumers',
+    'group.id': 'coordinates_consumers1',
     'auto.offset.reset': 'earliest',
     'enable.auto.commit': True,
-    # 'max.poll.records': 5,
-    'heartbeat.interval.ms': 25000,
-    'max.poll.interval.ms': 190000,
-    'session.timeout.ms': 180000,
+    'heartbeat.interval.ms': 3000,
 }
 
 
 class KafkaCoordinatesConsumer:
     """
     """
-    def __init__(self, cons_qty, poll_delay=1.0):
+
+    def __init__(self, cons_qty, poll_delay=1):
         self.cons_qty = cons_qty
         self.poll_delay = poll_delay
+        self.cons_user_id_map = defaultdict(set)
+        self.consumers = [Consumer(conf) for _ in range(self.cons_qty)]
+        for cons in self.consumers:
+            cons.subscribe(['coordinates'])
 
-    async def _one_consumer(self, number):
+    async def _one_consumer(self, number, consumer):
         logger.info(f'Starting consumer # {number}')
-        # await asyncio.sleep(3.0)
-        consumer = Consumer(conf)
-        consumer.subscribe(['coordinates'])
+
         while True:
-            msg = consumer.poll()
+            await asyncio.sleep(0.0)
+            msg = consumer.poll(0.1)
             if msg is None:
+                logger.warning(f'Consumer # {number}, Could not get message')
                 await asyncio.sleep(self.poll_delay)
+                continue
             elif err := msg.error():
-                print(f"Consumer error happened: {err}")
+                logger.error(f"Consumer error happened: {err}")
                 continue
             else:
                 logger.info(
@@ -43,11 +46,18 @@ class KafkaCoordinatesConsumer:
                     f' offset # {msg.offset()},'
                     f' value - {msg.value()}'
                 )
-                # await asyncio.sleep(1.0)
+                self.cons_user_id_map[number].add(msg.key())
 
     async def consume(self):
-        await asyncio.gather(
-            *(self._one_consumer(num) for num in range(self.cons_qty))
-        )
+        coros = []
+        for num, cons in enumerate(self.consumers):
+            coro = self._one_consumer(num, cons)
+            coros.append(coro)
 
+        try:
+            await asyncio.gather(*coros)
+        finally:
+            for cons in self.consumers:
+                cons.close()
+                logger.info(f'Closing consumer {cons}')
 
